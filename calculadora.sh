eacbded569a03aa4215d826d7a96e69c383144c4
ArielMn22 <arielpaixao10@gmail.com>
2017-09-14 21:42:28 -0300

    Explicação

--- calculadora.sh
+++ calculadora.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-clear
+clear 						#Limpa a tela
 
-DESEJAC=sim
+DESEJAC=sim 					#Estabelece o conteúdo da variável  
 
-function pergunta()
+function pergunta() 				#Início da função
 {
-	echo 'Deseja continuar?'
-	read DESEJAC
+	echo 'Deseja continuar?' 		#Imprime na tela 
+	read DESEJAC 				#Grava a variável
 }
-while [ $DESEJAC == sim ]
+
+while [ $DESEJAC == sim ] 			#Início da while
 do
-	echo 'Digite um cálculo:'
-	read CALCULO
-	clear
-	RESULTADO=$(($CALCULO))
-	echo 'O resultado é:'
-	echo $RESULTADO
-	pergunta
-done
+	echo 'Digite um cálculo:' 		#Imprima na tela
+	read CALCULO 				#Grava a variável
+	clear 					#Limpa a tela
+	RESULTADO=$(($CALCULO)) 		#Realiza o cálculo e grava na variável
+	echo 'O resultado é:' 			#Imprime na tela
+	echo $RESULTADO 			#Imprime na tela
+	pergunta 				#Chama a função
+done 						#Fim da while
