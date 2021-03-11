17ddcf9f5eb9f0ff85ed3d07e62bb9a3eba62ea9
arielmn22 <arielpaixao10@gmail.com>
2021-03-11 08:12:23 -0600

    Adicionando linha de LOG

--- Technical/firewall.restritivo.2.sh
+++ Technical/firewall.restritivo.2.sh
@@ -23,7 +23,8 @@
 	#iptables -A OUTPUT -p tcp --sport 2222 -j ACCEPT
 
 # Dropar "ICMP" do SRVWEB-DENVER para CLIENTE, quando for acionado gere log dizendo "Acesso ao Cliente bloqueado"
-	iptables -A FORWARD -p icmp -s 172.31.100.252 -d 10.10.100.1 --log-prefix "Acesso ao Cliente bloqueado" -j DROP
+	iptables -A LOG -p icmp -s 172.31.100.252 -d 10.10.100.1 --log-prefix "Acesso ao Cliente bloqueado" -j DROP
+	iptables -A FORWARD -p icmp -s 172.31.100.252 -d 10.10.100.1 -j DROP
 
 # LIBERAR O ICMP PARA TODO CENARIO
 	iptables -A INPUT -p icmp -j ACCEPT
@@ -56,12 +57,11 @@
 
 # LIBERAR PROTOCOLO FTP
 	iptables -A INPUT -p tcp --match multiport --dport 20,21 -j ACCEPT	
-	iptables -A INPUT -p tcp --match multiport --dport 20,21 -j ACCEPT	
+	iptables -A INPUT -p tcp --match multiport --sport 20,21 -j ACCEPT	
 	iptables -A OUTPUT -p tcp --match multiport --dport 20,21 -j ACCEPT	
 	iptables -A OUTPUT -p tcp --match multiport --sport 20,21 -j ACCEPT	
 	iptables -A FORWARD -p tcp --match multiport --dport 20,21 -j ACCEPT	
 	iptables -A FORWARD -p tcp --match multiport --sport 20,21 -j ACCEPT	
-
 
 # LIBERAR COMPARTILHAMENTO DE ARQUIVOS DO SRVDV-NAIROBI SOMENTE PARA O CLIENTE
 	iptables -A FORWARD -p tcp -s 172.31.100.252 --match multiport --dport 20,21 -j DROP
