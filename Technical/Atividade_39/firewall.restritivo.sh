23dd55affdd61cca2cd487e70e53b3958a9a5e8c
arielmn22 <arielpaixao10@gmail.com>
2021-03-12 07:48:12 -0600

    more rules

--- Technical/Atividade_39/firewall.restritivo.sh
+++ Technical/Atividade_39/firewall.restritivo.sh
@@ -66,6 +66,8 @@ iptables -A FORWARD -p tcp -s 172.31.100.253 --match multiport --dport 80,443 -j
 iptables -A FORWARD -p tcp -d 172.31.100.253 --match multiport --dport 80,443 -j DROP
 iptables -A FORWARD -p tcp --match multiport --dport 80,443 -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --dport 80,443 -j ACCEPT
+iptables -A INPUT -p tcp --match multiport --sport 80,443 -j ACCEPT
+iptables -A OUTPUT -p tcp --match multiport --dport 80,443 -j ACCEPT
 iptables -A OUTPUT -p tcp --match multiport --sport 80,443 -j ACCEPT
 MESSAGE="Liberar navegação para todos dispositivos exceto SRVWEB-DENVER - OK!"
 awaitInput
